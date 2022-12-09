e3677ba84fb7d6d6f6fc08cb5279454f8f6778fe
John Webster <johnwebster@Johns-MBP-2.hsd1.wa.comcast.net>
2022-12-09 11:23:49 -0800

    empty message updated with button

--- src/components/empty-message/empty-message.ts
+++ src/components/empty-message/empty-message.ts
@@ -1,12 +1,22 @@
 import {bindable} from 'aurelia-framework';
+import './empty-message.css';
 
 export class EmptyMessage {
   @bindable message: string;
   @bindable showmore: boolean;
 
-  activate () {
-    console.log('message: ', this.message, 'show more: ', this.showmore);
-  }
+  activate () {}
   
   constructor() {}
+
+  handleUserAction (evt: Event):void {
+    evt.preventDefault();
+    try {
+      const focusEl:HTMLInputElement = document.querySelector('.suggest-city-control > input');  
+      focusEl.focus();
+    } catch (err) {
+      console.error('unable to focus button');
+    }
+
+  }
 }
